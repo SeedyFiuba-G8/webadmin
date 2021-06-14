eee4ce9b9040751fc8968c43935c4405935427b8
TaielC <43725160+TaielC@users.noreply.github.com>
2021-06-14 18:03:45 -0300

    Working list all users

--- src/api/usersQuery.js
+++ src/api/usersQuery.js
@@ -1,8 +1,8 @@
-import apiProvider from './utilities/fakeProvider';
+import apiProvider from './utilities/provider';
 
 export async function getAllUsers() {
     try {
-        const user = await apiProvider.get('users');
+        const user = await apiProvider.get('user');
         return user.users;
     } catch (error) {
         console.error(error);
