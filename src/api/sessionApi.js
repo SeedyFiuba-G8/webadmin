fa83504324e5529b92c3abc630b4d0dc48e7e381
tonyhuang07 <yhuang@fi.uba.ar>
2021-06-27 20:16:08 -0300

    [MOD] Changed url of API

--- src/api/sessionApi.js
+++ src/api/sessionApi.js
@@ -3,7 +3,7 @@ import apiProvider from './utilities/provider';
 export default async function createSession(email, password) {
     console.log(`Requesting session for: ${email}`);
     try {
-        const apiResponse = await apiProvider.post('admin/session', {
+        const apiResponse = await apiProvider.post('admins/session', {
             email: email,
             password: password,
         });
