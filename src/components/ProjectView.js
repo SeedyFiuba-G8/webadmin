4b81cdbd0e434bf30cb90b9c5a0911ee9aa64e2d
tonyhuang07 <yhuang@fi.uba.ar>
2021-06-27 20:17:26 -0300

    [ADD] Second version of project's visualization

--- src/components/ProjectView.js
+++ src/components/ProjectView.js
@@ -1,6 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, React } from 'react';
 import { getProject } from '../api/projectsQuery';
-import { Paper } from '@material-ui/core';
+import ProjectStyle from './ProjectStyle';
+import ProjectDetail from './ProjectDetail';
+import { CssBaseline, Grid, Container } from '@material-ui/core';
+
+import TemporaryImg from '../assets/temporary-img.jpg';
 
 export default function ProjectView(props) {
     const [project, setProject] = useState([]);
@@ -8,6 +12,36 @@ export default function ProjectView(props) {
         const loadProjects = async () => setProject(await getProject(props.id));
         loadProjects();
     }, [props.id]);
-    console.log(project.id);
-    return <Paper />;
+    // console.log(project.id);
+
+    const projectStyle = {
+        title: project.title,
+        description: project.description,
+        image: TemporaryImg,
+        imgText: 'main image description',
+    };
+
+    return (
+        <>
+            <CssBaseline />
+            <Container maxWidth="lg">
+                <ProjectStyle post={projectStyle} />
+                <Grid container spacing={2}>
+                    <ProjectDetail title="Objective" info={project.objective} />
+                    <ProjectDetail title="Country" info={project.country} />
+                    <ProjectDetail title="City" info={project.city} />
+                </Grid>
+                <Grid container spacing={2}>
+                    <ProjectDetail
+                        title="Published on"
+                        info={project.publishedOn}
+                    />
+                    <ProjectDetail
+                        title="Finalized by"
+                        info={project.finalizedBy}
+                    />
+                </Grid>
+            </Container>
+        </>
+    );
 }
