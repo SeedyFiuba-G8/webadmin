fb43588a963a49604ba28e5db9be2aaf10541eb2
TaielC <43725160+TaielC@users.noreply.github.com>
2021-07-09 10:46:10 -0300

    Code cleanup

--- src/components/ProjectsTable.js
+++ src/components/ProjectsTable.js
@@ -9,19 +9,31 @@ function ProjectsTable(props) {
     useEffect(() => {
         const loadProjects = async () => setProjects(await getProjects(config));
         loadProjects();
-    }, [config]);
+    });
 
     function onRowClickAction(rowData) {
         const id = rowData[0];
         props.history.push('projects/' + id);
     }
 
+    function updateConfig(update) {
+        setConfig({ ...config, ...update });
+    }
+
     function changePage(page) {
-        setConfig({ ...config, currentPage: page });
+        updateConfig({ currentPage: page });
     }
 
     function changeRowsPerPage(rows) {
-        setConfig({ ...config, rowsPerPage: rows });
+        updateConfig({ rowsPerPage: rows });
+    }
+
+    function sortByColumn(column, direction) {
+        console.log(`Columns sort direction: ${direction}`);
+        updateConfig({
+            sortColumn: column,
+            sortAsc: direction === 'asc' ? true : false,
+        });
     }
 
     return (
@@ -35,9 +47,12 @@ function ProjectsTable(props) {
                 resizableColumns: true,
                 page: config.currentPage,
                 rowsPerPage: config.rowsPerPage,
+                jumpToPage: true,
+                download: false,
                 rowsPerPageOptions: [3, 5, 10, 15, 50],
                 onChangePage: changePage,
                 onChangeRowsPerPage: changeRowsPerPage,
+                onColumnSortChange: sortByColumn,
             }}
         />
     );
