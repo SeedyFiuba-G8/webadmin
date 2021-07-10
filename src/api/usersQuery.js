71512b140fe9fc7c6a356dfc928bc64c4ad178a2
tonyhuang07 <yhuang@fi.uba.ar>
2021-07-10 19:54:26 -0300

    [ADD] Add get user api

--- src/api/usersQuery.js
+++ src/api/usersQuery.js
@@ -10,3 +10,14 @@ export async function getAllUsers() {
         return [];
     }
 }
+
+export async function getUser(id) {
+    try {
+        const user = await apiProvider.get(`users/${id}`);
+        return user;
+    } catch (error) {
+        console.error(error);
+        console.log("Couldn't get user.");
+        return [];
+    }
+}
